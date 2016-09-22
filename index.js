20a8bdc7cb551941f335bdae7108fae25104ce91
Sylvain Ducamp <sylvain.ducamp@gammadia.ch>
2016-09-22 11:05:25 +0200

    - Connection to couchbase

--- index.js
+++ index.js
@@ -3,47 +3,17 @@ module.exports = function (app) {
 	'use strict';
 
 	var logger = app.logger && app.logger.child({component: 'Database'}),
-		nano = require('nano'),
-		db = null,
-		Agentkeepalive = require('agentkeepalive'),
-		agent = null;
-
-	agent = new Agentkeepalive({
-		maxSockets: app.config.get('db:sockets') || 32,
-		maxKeepAliveRequests: 0,
-		maxKeepAliveTime: 30000
-	});
-
-	db = nano({
-		url:	app.config.get('db:url'),
-		log:	function (id, args) {
-			if (!logger) {
-				return;
-			}
-
-			if (args[0].method) {
-				logger.debug('%s - %s', args[0].method, args[0].uri);
-			}
-
-			if (args[0].err === null) {
-				logger.debug('[%s] - %s', args[0].headers['status-code'], args[0].body._id);
-			}
-		},
-		request_defaults: {
-			agent: agent
-		}
-	});
-
-	if (db instanceof Error) {
-		if (logger) {
-			app.logger.fatal('Erreur lors de la connection à la base de données %s', app.config.get('db:url'));
-		}
-		return new Error({code: 'DATABASE_CONNECT_FAIL'});
-	}
-
-	if (logger) {
-		app.logger.info('Connecté à la base de données %s', app.config.get('db:url'));
-	}
-
-    return db;
+		couchbase = require('couchbase'),
+        cluster = new couchbase.Cluster('couchbase://' + app.config.get('couchbase:url')),
+        bucket = cluster.openBucket(app.config.get('couchbase:bucket'), function (err) {
+            if (err) {
+                if (logger) {
+                    app.logger.fatal('Erreur lors de la connection à la base de données %s', app.config.get('db:url'));
+                }
+                return new Error({code: 'DATABASE_CONNECT_FAIL'});
+            } else {
+                app.logger.info('Connecté à la base de données %s', app.config.get('db:url'));
+            }
+        });
+    return bucket;
 };
