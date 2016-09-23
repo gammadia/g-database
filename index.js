5c2079ae4b74f5e60da27b7fe78e587e3c6cee90
Sylvain Ducamp <sylvain.ducamp@gammadia.ch>
2016-09-23 09:51:36 +0200

    - New database properties

--- index.js
+++ index.js
@@ -2,18 +2,31 @@
 module.exports = function (app) {
 	'use strict';
 
-	var logger = app.logger && app.logger.child({component: 'Database'}),
-		couchbase = require('couchbase'),
-        cluster = new couchbase.Cluster('couchbase://' + app.config.get('couchbase:url')),
-        bucket = cluster.openBucket(app.config.get('couchbase:bucket'), function (err) {
-            if (err) {
-                if (logger) {
-                    app.logger.fatal('Erreur lors de la connection à la base de données %s', app.config.get('db:url'));
-                }
-                return new Error({code: 'DATABASE_CONNECT_FAIL'});
-            } else {
-                app.logger.info('Connecté à la base de données %s', app.config.get('db:url'));
+	var logger      = app.logger && app.logger.child({component: 'Database'}),
+        couchbase   = require('couchbase'),
+        cluster     = null,
+        bucket      = null,
+        database    = {
+            set: function (host, bucketName) {
+                cluster = new couchbase.Cluster('couchbase://' + host);
+                bucket = cluster.openBucket(bucketName, function (err) {
+                    if (err) {
+                        if (logger) {
+                            app.logger.fatal('Erreur lors de la connection à la base de données %s', app.config.get('db:url'));
+                        }
+                        return new Error({code: 'DATABASE_CONNECT_FAIL'});
+                    } else {
+                        app.logger.info('Connecté à la base de données %s', app.config.get('db:url'));
+                    }
+                });
+                return bucket;
+            },
+            get: function () {
+                return bucket;
+            },
+            getViewQuery: function () {
+                return couchbase.ViewQuery;
             }
-        });
-    return bucket;
+        };
+    return database;
 };
