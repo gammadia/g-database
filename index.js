e955cceb5118c5ded33d6415200df60ceb2a4bfa
sylvain <sylvain.ducamp@gammadia.ch>
2022-08-19 14:37:51 +0200

    Retry connection to the database

--- index.js
+++ index.js
@@ -1,71 +1,51 @@
 /*jslint node: true, nomen: true, white: true */
 const couchbase = require("couchbase");
 module.exports = function (app) {
-	'use strict';
+  'use strict';
 
-  var logger      = app.logger && app.logger.child({component: 'Database'}),
-    couchbase   = require('couchbase'),
-        crypto      = require('crypto-js'),
-        cluster     = null,
-        bucket      = null,
-        database    = {
-            init: function (host, bucketName, username, password) {
-                cluster = new couchbase.Cluster('couchbase://' + host);
-                cluster.authenticate(username, password);
+  let logger = app.logger && app.logger.child({component: 'Database'}),
+    couchbase = require('couchbase'),
+    crypto = require('crypto-js'),
+    cluster = null,
+    bucket = null,
+    retry = 0;
 
-                function connect() {
-                  return new Promise((resolve, reject) => {
-                    let b = cluster.openBucket(bucketName, function (err) {
-                      if (err) {
-                        return reject(err)
-                      }
+  return {
+    init: function (host, bucketName, username, password) {
+      if (retry > 10) {
+        logger.error("Unable to connect to the database.")
+        return;
+      }
 
-                      resolve(b)
-                    });
-                  });
-                }
+      cluster = new couchbase.Cluster('couchbase://' + host);
+      cluster.authenticate(username, password);
+      bucket = cluster.openBucket(bucketName, function (err) {
+        if (err) {
+          retry++;
+          setTimeout(() => {
+            this.init(host, bucketName, username, password)
+          }, 2000);
+        }
+      });
+    },
+    get: function () {
+      return cluster;
+    },
+    getViewQuery: function () {
+      return couchbase.ViewQuery;
+    },
+    getN1qlQuery: function () {
+      return couchbase.N1qlQuery;
+    },
+    getSpatialQuery: function () {
+      return couchbase.SpatialQuery;
+    },
+    getKey: function (secret) {
+      var date = (new Date()).valueOf().toString(),
+        random = Math.random().toString(),
+        secret = secret || '';
 
-                let retry = 0;
-                function establishConnection() {
-                  if (retry > 10) {
-                    app.logger.error("Unable to connect to the database")
-
-                    return
-                  }
-                  const c = connect();
-                  c.then(b => {
-                    app.logger.info("Connected to the database")
-
-                    bucket = b
-                  }).catch(err => {
-                    app.logger.error(err)
-
-                    retry++;
-                    setTimeout(establishConnection, 2000)
-                  });
-                }
-
-                establishConnection();
-            },
-            get: function () {
-                return bucket;
-            },
-            getViewQuery: function () {
-                return couchbase.ViewQuery;
-            },
-            getN1qlQuery: function () {
-                return couchbase.N1qlQuery;
-            },
-            getSpatialQuery: function () {
-                return couchbase.SpatialQuery;
-            },
-            getKey: function (secret) {
-                var date = (new Date()).valueOf().toString(),
-                    random = Math.random().toString(),
-                    secret = secret || '';
-
-                return crypto.HmacSHA256(date + random, secret).toString(crypto.enc.Hex);
-            }
-        };
-    return database;
+      return crypto.HmacSHA256(date + random, secret).toString(crypto.enc.Hex);
+    }
+  };
 };
