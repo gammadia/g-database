0c56d6cfd8d4eb0bbfe5ef293ca67f1205607383
sylvain <sylvain.ducamp@gammadia.ch>
2022-08-25 09:27:03 +0200

    Retry connection to the database

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ module.exports = function (app) {
       return bucket;
     },
     get: function () {
-      return cluster;
+      return bucket;
     },
     getViewQuery: function () {
       return couchbase.ViewQuery;
