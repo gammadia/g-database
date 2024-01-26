1789cf9be7ece5d30f5eb68bcff44e4db856df8c
sducamp <sducamp@gmail.com>
2024-01-26 18:17:09 +0100

    Couchbase SDK 4.2.9
    Create the ViewQuery class.
    It's an adapter for the legacy couchbase SDK interface.
    
    Use the native crypto module.

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # g-database
 Node module to manage the Couchbase database connection.
 
-This version is compatible with Node 20.11.0 and use the Couchebase SDK 3.2.0.
+This version is compatible with Node 20.11.0 and use the Couchebase SDK 4.2.
 
 ## Installation
-```npm install g-database@3.2.0```
+```npm install g-database@4.2```
 
 ## Dependencies
-- [couchbase](https://www.npmjs.com/package/couchbase) (3.2.0)
-- [crypte](https://www.npmjs.com/package/crypto-js) (3.1.9)
+- [couchbase](https://www.npmjs.com/package/couchbase) (4.2)
--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 'use strict';
 
-const {ViewScanConsistency, ViewOrdering} = require("couchbase/dist/viewtypes");
 const couchbase = require("couchbase");
+
 module.exports = function (app) {
   'use strict';
 
-  const couchbase = require('couchbase');
-
-  let crypto = require('crypto-js'),
+  let crypto,
     cluster = null,
     bucket = null;
 
+  try {
+    crypto = require('node:crypto');
+  } catch (err) {
+    console.log('crypto support is disabled!');
+    console.log(err)
+
+    return;
+  }
+
+  const couchbase = require('couchbase');
+
   return {
-    ViewScanConsistency: {
-      Update: {
-        BEFORE: couchbase.ViewScanConsistency.RequestPlus,
-        AFTER: couchbase.ViewScanConsistency.UpdateAfter,
-        NONE: couchbase.ViewScanConsistency.NotBounded
-      }
-    },
     ViewOrdering: {
       Ascending: couchbase.ViewOrdering.Ascending,
       Descending: couchbase.ViewOrdering.Descending
@@ -31,8 +33,6 @@ module.exports = function (app) {
      * @param {string} bucketName
      * @param {string} username
      * @param {string} password
-     *
-     * @returns {Promise}
      */
     init: async function (host, bucketName, username, password) {
       cluster = await couchbase.connect('couchbase://' + host, {
@@ -42,7 +42,7 @@ module.exports = function (app) {
 
       bucket = cluster.bucket(bucketName);
 
-      return
+      return;
     },
     close: function (callback) {
       cluster.close(callback)
@@ -50,16 +50,137 @@ module.exports = function (app) {
     get: function () {
       return bucket;
     },
+
+    /**
+     * @return {ViewQuery}
+     */
+    getViewQuery: function () {
+      return new ViewQuery();
+    },
+
     /**
      * @param {string} secret
      * @returns {string}
      */
     getKey: function (secret) {
-      const date = (new Date()).valueOf().toString(),
-        random = Math.random().toString(),
-        s = secret || '';
+      const date = (new Date()).valueOf().toString();
+      const array = new Uint32Array(10);
+      const random = crypto.getRandomValues(array);
+      const s = secret || '';
+      const {createHmac} = require('node:crypto');
 
-      return crypto.HmacSHA256(date + random, s).toString(crypto.enc.Hex);
+      return createHmac('sha256', s).update(date + random).digest('hex');
     }
   };
 };
+
+class ViewQuery {
+  constructor() {
+    this._designDoc = "";
+    this._viewName = ""
+    this._options = {range: {start: null, end: null}};
+
+    this.Update = {
+      BEFORE: couchbase.ViewScanConsistency.RequestPlus,
+      AFTER: couchbase.ViewScanConsistency.UpdateAfter,
+      NONE: couchbase.ViewScanConsistency.NotBounded
+    }
+  }
+
+  from(designDoc, viewName) {
+    this._designDoc = designDoc;
+    this._viewName = viewName;
+
+    return this;
+  }
+
+  key(key) {
+    this._options.key = key;
+
+    return this;
+  }
+
+  keys(keys) {
+    this._options.keys = keys;
+
+    return this;
+  }
+
+  stale(stale) {
+    this._options.scanConsistency = stale;
+
+    return this;
+  }
+
+  limit(limit) {
+    this._options.limit = limit;
+
+    return this;
+  }
+
+  skip(step) {
+    this._options.skip = step;
+
+    return this;
+  }
+
+  range(start, end) {
+    this._options.range.start = start;
+    this._options.range.end = end;
+
+    return this;
+  }
+
+  reduce(reduce) {
+    this._options.reduce = reduce;
+
+    return this;
+  }
+
+  custom(options) {
+      if (options.group_level !== undefined) {
+        this._options.group_level = options.group_level;
+      }
+
+      if (options.startkey !== undefined) {
+        this._options.range.start = JSON.parse(options.startkey);
+      }
+
+      if (options.endkey !== undefined) {
+        this._options.range.end = JSON.parse(options.endkey);
+      }
+
+      if (options.descending !== undefined) {
+        this._options.order = options.descending ? couchbase.ViewOrdering.Descending : couchbase.ViewOrdering.Ascending;
+      }
+
+      if (options.reduce !== undefined) {
+        this._options.reduce = options.reduce;
+      }
+
+      if (options.key !== undefined) {
+        this._options.key = options.key;
+      }
+
+      // @deprecated
+      if (options.include_docs !== undefined) {
+        this._options.include_docs = options.include_docs;
+      }
+
+    return this;
+  }
+
+  getDesignDoc() {
+    return this._designDoc;
+  }
+
+  getViewName() {
+    return this._viewName;
+  }
+
+  getOptions() {
+    return this._options;
+  }
+}
+
+module.exports.ViewQuery = ViewQuery;
